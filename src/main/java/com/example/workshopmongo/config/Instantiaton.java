db65e3ad8f6916138c5c2b60e3d0f7e6f69ed0c4
Gabrielfreitas3110 <gabriel.freitas3110@gmail.com>
2021-08-31 11:53:07 -0230

    Configuration of instanciation of database

---
+++ src/main/java/com/example/workshopmongo/config/Instantiaton.java
@@ -0,0 +1,26 @@
+package com.example.workshopmongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.example.workshopmongo.domain.User;
+import com.example.workshopmongo.repository.UserRepository;
+
+@Configuration
+public class Instantiaton implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		userRepository.deleteAll();
+		User maria = new User(null, "Maria", "maria@gmail.com");
+		User gabriel = new User(null, "Gabriel", "gabrieel@gmail.com");
+		userRepository.saveAll(Arrays.asList(maria, gabriel));		
+	}
+}
