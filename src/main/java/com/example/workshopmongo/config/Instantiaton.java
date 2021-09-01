4c947834224971fbc10acd3290f9e1f8581f453f
Gabrielfreitas3110 <gabriel.freitas3110@gmail.com>
2021-09-01 15:54:42 -0230

    Referenciating Post of a User

--- src/main/java/com/example/workshopmongo/config/Instantiaton.java
+++ src/main/java/com/example/workshopmongo/config/Instantiaton.java
@@ -39,5 +39,8 @@ public class Instantiaton implements CommandLineRunner {
 		Post post1 = new Post(null, sdf.parse("21/03/2021"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2021"), "Bom dia", "Acordei feliz !", new AuthorDTO(maria));
 		postRepository.saveAll(Arrays.asList(post1, post2));
+		
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		userRepository.save(maria);
 	}
 }
