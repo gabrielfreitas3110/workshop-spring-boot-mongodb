18010d0f74d0cf11f1952eaba836d931d9c8627a
Gabrielfreitas3110 <gabriel.freitas3110@gmail.com>
2021-09-01 16:11:30 -0230

    Post by Id

---
+++ src/main/java/com/example/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.example.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.workshopmongo.domain.Post;
+import com.example.workshopmongo.repository.PostRepository;
+import com.example.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repository;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado. Id: " + id));
+	}
+}
