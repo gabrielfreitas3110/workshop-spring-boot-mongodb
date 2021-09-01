65804747a320e41fd896afae50c73ddc0a9d03c1
Gabrielfreitas3110 <gabriel.freitas3110@gmail.com>
2021-09-01 18:34:37 -0230

    Query with several criterias

--- src/main/java/com/example/workshopmongo/services/PostService.java
+++ src/main/java/com/example/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.example.workshopmongo.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -23,5 +24,11 @@ public class PostService {
 	
 	public List<Post> findByTtitle(String text) {
 		return repository.searchTitle(text);
+	}
+	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+		minDate = new Date(minDate.getTime());
+		maxDate = new Date(maxDate.getTime());
+		return repository.fullSearch(text, minDate, maxDate);
 	}
 }
