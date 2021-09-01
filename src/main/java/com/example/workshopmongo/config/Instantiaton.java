0664a9da13bfc7b43bd16d3ed2327dca6fe80beb
Gabrielfreitas3110 <gabriel.freitas3110@gmail.com>
2021-09-01 17:48:15 -0230

    Simple query with regex

--- src/main/java/com/example/workshopmongo/config/Instantiaton.java
+++ src/main/java/com/example/workshopmongo/config/Instantiaton.java
@@ -31,25 +31,24 @@ public class Instantiaton implements CommandLineRunner {
 		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 		
 		userRepository.deleteAll();
-		User maria = new User(null, "Maria", "maria@gmail.com");
 		User gabriel = new User(null, "Gabriel", "gabriel@gmail.com");
 		User washington = new User(null, "Washington", "washington@gmail.com");
-		userRepository.saveAll(Arrays.asList(maria, gabriel, washington));	
+		User maria = new User(null, "Maria", "maria@gmail.com");
+		userRepository.saveAll(Arrays.asList(gabriel, washington, maria));	
 
 		postRepository.deleteAll();
-		Post post1 = new Post(null, sdf.parse("21/03/2021"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
-		Post post2 = new Post(null, sdf.parse("23/03/2021"), "Bom dia", "Acordei feliz !", new AuthorDTO(maria));
+		Post post1 = new Post(null, sdf.parse("21/03/2021"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(gabriel));
+		Post post2 = new Post(null, sdf.parse("23/03/2021"), "Bom dia", "Acordei feliz!", new AuthorDTO(gabriel));
 
-		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2021"), new AuthorDTO(gabriel));
-		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2021"), new AuthorDTO(washington));
-		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2021"), new AuthorDTO(gabriel));
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2021"), new AuthorDTO(washington));
+		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2021"), new AuthorDTO(maria));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2021"), new AuthorDTO(washington));
 		
 		post1.getComments().addAll(Arrays.asList(c1, c2));
 		post2.getComments().add(c3);
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
-		
-		maria.getPosts().addAll(Arrays.asList(post1, post2));
-		userRepository.save(maria);
+		gabriel.getPosts().addAll(Arrays.asList(post1, post2));
+		userRepository.save(gabriel);
 	}
 }
